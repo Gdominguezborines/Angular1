5c08fc219a8ca641415bd83b2211fb8c5e60940a
Adrian <adriancostasgarcia1998@gmail.com>
2024-12-05 09:30:12 +0100

    Rutas creadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TresEnRayaComponent } from './tres-en-raya/tres-en-raya.component';
+import { CalcComponent } from './calc/calc.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'index', pathMatch:'full'},
+  {path:'TresEnRaya',component:TresEnRayaComponent},
+  {path:'calc', component:CalcComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
